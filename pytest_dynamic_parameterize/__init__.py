671b61b1154a40229601d419fecd8a91eacbaa19
avizaguri-macbook <avi210892@gmail.com>
2025-07-13 22:24:06 +0300

    Fix string formatting in logger initialization in __init__.py

--- pytest_dynamic_parameterize/__init__.py
+++ pytest_dynamic_parameterize/__init__.py
@@ -2,4 +2,4 @@ import logging
 
 from custom_python_logger import get_logger
 
-logger = get_logger(project_name='pytest-plugins', log_level=logging.DEBUG)
+logger = get_logger(project_name="pytest-plugins", log_level=logging.DEBUG)
