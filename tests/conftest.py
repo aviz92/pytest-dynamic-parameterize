f2a4d2754d70df87c7aecbc0a184d3d9d19a0380
avizaguri-macbook <avi210892@gmail.com>
2025-12-05 18:01:38 +0200

    Add GitHub Actions workflows for linting and testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
 from custom_python_logger import build_logger
 
 logger = build_logger(project_name="pytest-dynamic-parameterize", log_file=True)
+logger.info("Hello world")
+logger.warning("Hello world")
+logger.error("Hello world")
+logger.debug("Hello world")
