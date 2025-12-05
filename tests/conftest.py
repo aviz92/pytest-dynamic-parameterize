e6565e0f8be68786ed23525e280965ac80c58898
avizaguri-macbook <avi210892@gmail.com>
2025-12-05 17:44:42 +0200

    Update pre-commit configuration, add pylint settings for tests, and enhance parameterized tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,3 @@
 from custom_python_logger import build_logger
 
-pytest_plugins = [
-    "pytest_dynamic_parameterize.dynamic_parameterize",
-]
-
 logger = build_logger(project_name="pytest-dynamic-parameterize", log_file=True)
--- tests/parameterize_functions/my_params.py
+++ tests/parameterize_functions/my_params.py
@@ -1,12 +1,15 @@
-def my_params(config, some_param=None) -> list[tuple]:  # pylint: disable=:W0613
+from _pytest.config import Config
+
+
+def my_params(config: Config, some_param: str = None) -> list[tuple]:  # pylint: disable=:W0613
     # Example: generate parameters dynamically, using an optional argument
     if some_param == "special":
         return [
             (10, 20, 30),
-            (40, 50, 60),
+            (40, 20, 60),
         ]
     return [
         (1, 2, 3),
-        (4, 5, 6),
-        (7, 8, 9),
+        (4, 2, 6),
+        (7, 2, 9),
     ]
